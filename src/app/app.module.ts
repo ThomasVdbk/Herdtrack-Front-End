f8355da18dabc76e9ee2fa60888e3dafcd97b84e
Thomas Screve <thomas-scrve@laposte.net>
2022-07-12 09:32:21 +0200

    création du component formulaire-register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormulaireRegisterComponent } from './components/formulaire-register/formulaire-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormulaireRegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/formulaire-register/formulaire-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire-register',
+  templateUrl: './formulaire-register.component.html',
+  styleUrls: ['./formulaire-register.component.scss']
+})
+export class FormulaireRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
